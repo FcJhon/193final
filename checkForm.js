1c7d6377e43d26f97ff77db19ae0ca07f28c1194
FcJhon <77075109+FcJhon@users.noreply.github.com>
2021-06-09 23:19:03 -0700

    update

--- checkForm.js
+++ checkForm.js
@@ -1,58 +1,23 @@
-function checkForm() {
-    var areFormErrors = false;
-    var errorMessages = "";
- 
-    errorMessages += "<ul>";
- 
-    var fullNameElement = document.getElementById("fullname");
-    if (fullNameElement.value.length === 0) {
-       errorMessages += "<li>Missing full name.</li>";
-       fullNameElement.classList.add("error");
-       areFormErrors = true;
-    }
-    else {
-       fullNameElement.classList.remove("error");
-    }
+var phoneEl= document.getElementById('phone');
+var message = '';
+var phonePt= /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+var error= false;
 
-    var addressElement= document.getElementById('address');
-    if (addressElement.value.length === 0){
-        errorMessages +='<li>Missing Address</li>';
-        addressElement.classList.add("error");
-        areFormErrors = true;
-    }
-    else{
-        addressElement.classList.remove('error')
-    }
+document.getElementById('submit').addEventListener('click', check);
 
-
-    var phoneElement = document.getElementById('phone');
-    var phonePt= /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/;
-    if (phoneElement.value.length===0 ||  !phonePt.test(phoneElement.value)){
-        errorMessages+='<li>Invalid Phone Number</li>';
-        phoneElement.classList.add('error');
-        areFormErrors=true;
-    }
-    else{
-        phoneElement.classList.remove('error');
-    }
- 
-    
- 
-    errorMessages += "</ul>";
- 
-    if (areFormErrors) {
-       document.getElementById("error").innerHTML = errorMessages;
-       
-    }
-    else {
-       document.getElementById("error").style.display = "none";
-    }
- }
- 
- document.getElementById("submit").addEventListener("click", function(event) {
-    checkForm();
-    
- 
-    event.preventDefault();
- });
- 
+function check(event){
+   if(phoneEl.value.length===0 || !phonePt.test(phoneEl.value)){
+      message= 'Invalid phone Number!';
+      document.getElementById('error').innerHTML= message;
+      error=true;
+   }
+   else{
+      error=false;
+      document.getElementById('error').style.display='none';
+      
+   }
+   
+   if(error){
+      event.preventDefault();
+   }
+}
---
+++ checkPay.js
@@ -0,0 +1,8 @@
+var cardNumber = document.getElementById('cardn');
+var expirD = document.getElementById('exd');
+var cvvn = document.getElementById('cvv');
+var amount = document.getElementById('amt');
+
+var message= '' ;
+var error = false;
+
