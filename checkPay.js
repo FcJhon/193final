c775baea7ae9d34eb9965463786752121fa4ee88
FcJhon <77075109+FcJhon@users.noreply.github.com>
2021-06-11 20:18:23 -0700

    update

--- checkPay.js
+++
@@ -1,8 +0,0 @@
-var cardNumber = document.getElementById('cardn');
-var expirD = document.getElementById('exd');
-var cvvn = document.getElementById('cvv');
-var amount = document.getElementById('amt');
-
-var message= '' ;
-var error = false;
-
